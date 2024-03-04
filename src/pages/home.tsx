b21e15004f26e4f07e2e7cd0bec0bfedee7a791f
dpaulos6 <itzframepvp@outlook.com>
2024-03-04 09:53:58 +0000

    Discord Link Update

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <Navbar />
       <div className='w-screen h-screen flex flex-col gap-2 items-center justify-center'>
         <span className='text-5xl'>This is still being built</span>
-        <span className='text-lg'>You can join my <a href="https://discord.gg/JsDcjrPzf2/" className='text-primary'>discord</a> if you wanna see its progress</span>
+        <span className='text-lg'>You can join my <a href="https://discord.gg/Y7ujpKmmma" className='text-primary'>discord</a> if you wanna see its progress</span>
       </div>
     </>
   )

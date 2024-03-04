4c91e333b3989e2230d6ea3034b7850ee96c2626
Diogo Paulos <51181228+dpaulos6@users.noreply.github.com>
2024-03-04 00:35:50 +0000

    Home page warn

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -5,6 +5,10 @@ export default function Home() {
   return (
     <>
       <Navbar />
+      <div className='w-screen h-screen flex flex-col gap-2 items-center justify-center'>
+        <span className='text-5xl'>This is still being built</span>
+        <span className='text-lg'>You can join my <a href="https://discord.gg/JsDcjrPzf2/" className='text-primary'>discord</a> if you wanna see its progress</span>
+      </div>
     </>
   )
 }

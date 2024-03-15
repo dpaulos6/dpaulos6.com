0deb74cc9692b1ec42ac9acae3f95889b2ad0341
Diogo Paulos <51181228+dpaulos6@users.noreply.github.com>
2024-03-15 20:06:14 +0000

    Formatting

--- src/components/BackgroundAudio.tsx
+++ src/components/BackgroundAudio.tsx
@@ -1,34 +1,34 @@
-'use client'
-import React, { useEffect } from 'react'
-import AudioPlayer from './util/AudioPlayer'
+"use client";
+import React, { useEffect } from "react";
+import AudioPlayer from "./util/AudioPlayer";
 
 const BackgroundAudio: React.FC = () => {
   useEffect(() => {
-    const audio = document.getElementById('bgMusic') as HTMLAudioElement
+    const audio = document.getElementById("bgMusic") as HTMLAudioElement;
     if (audio) {
-      audio.volume = 0.1
+      audio.volume = 0.1;
     }
-  }, [])
+  }, []);
 
   function playMusic() {
-    const audio = document.getElementById('bgMusic') as HTMLAudioElement
+    const audio = document.getElementById("bgMusic") as HTMLAudioElement;
     if (audio) {
-      audio.play()
+      audio.play();
     }
   }
 
   return (
     <div className="fixed min-w-[332px] w-full p-4 xs:w-fit flex flex-col gap-2 right-1/2 translate-x-1/2 -bottom-[4.5rem] md:right-0 md:-translate-x-0 text-text hover:bottom-0 group transition-all !duration-500">
       <span className="w-full text-xs xs:text-sm md:text-base text-center absolute left-1/2 -translate-x-1/2 top-4 opacity-0 group-hover:-top-4 group-hover:opacity-100 transition-all !duration-500 select-none cursor-default">
-        Click{' '}
+        Click{" "}
         <button className="font-bold text-primary" onClick={playMusic}>
           play
-        </button>{' '}
+        </button>{" "}
         for an amazing experience
       </span>
       <AudioPlayer src="bgmusic.mp3" />
     </div>
-  )
-}
+  );
+};
 
-export default BackgroundAudio
+export default BackgroundAudio;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,56 +1,56 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     container: {
       center: true,
-      padding: '2rem',
+      padding: "2rem",
       screens: {
-        '2xl': '1400px'
-      }
+        "2xl": "1400px",
+      },
     },
     extend: {
       colors: {
-        primary: 'var(--primary)',
-        'primary-hover': 'var(--primary-hover)',
-        secondary: 'var(--secondary)',
-        'secondary-hover': 'var(--secondary-hover)',
-        accent: 'var(--accent)',
-        'accent-hover': 'var(--accent-hover)',
-        background: 'var(--background)',
-        'background-hover': 'var(--background-hover)',
-        'background-menu': 'var(--background-menu)',
-        'background-border': 'var(--background-border)',
-        text: 'var(--text)',
-        'text-hover': 'var(--text-hover)',
-        'audio-hover': 'var(--audio-hover)',
-        hover: 'var(--hover)'
+        primary: "var(--primary)",
+        "primary-hover": "var(--primary-hover)",
+        secondary: "var(--secondary)",
+        "secondary-hover": "var(--secondary-hover)",
+        accent: "var(--accent)",
+        "accent-hover": "var(--accent-hover)",
+        background: "var(--background)",
+        "background-hover": "var(--background-hover)",
+        "background-menu": "var(--background-menu)",
+        "background-border": "var(--background-border)",
+        text: "var(--text)",
+        "text-hover": "var(--text-hover)",
+        "audio-hover": "var(--audio-hover)",
+        hover: "var(--hover)",
       },
       screens: {
-        limit: '320px',
-        xs: '480px'
+        limit: "320px",
+        xs: "480px",
       },
       keyframes: {
-        'accordion-down': {
-          from: { height: '0' },
-          to: { height: 'var(--radix-accordion-content-height)' }
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
         },
-        'accordion-up': {
-          from: { height: 'var(--radix-accordion-content-height)' },
-          to: { height: '0' }
-        }
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
       },
       animation: {
-        'accordion-down': 'accordion-down 0.2s ease-out',
-        'accordion-up': 'accordion-up 0.2s ease-out'
-      }
-    }
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
   },
-  plugins: [require('tailwindcss-animate')]
-}
-export default config
+  plugins: [require("tailwindcss-animate")],
+};
+export default config;

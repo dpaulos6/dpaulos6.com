46d8a54b6e1bc7cd3c815533ef4ae9b989cb6ab8
dpaulos6 <itzframepvp@outlook.com>
2024-03-21 11:35:04 +0000

    mini patch

--- src/components/BackgroundAudio.tsx
+++ src/components/BackgroundAudio.tsx
@@ -98,7 +98,7 @@ const BackgroundAudio: React.FC = () => {
 
   return (
     <>
-      <div className='flex md:hidden w-full h-1/3 absolute bottom-0 z-50' {...handlers}></div>
+      <div className='flex md:hidden w-full h-1/3 absolute bottom-0 z-50 select-none' {...handlers}></div>
       <div ref={audioContainer} className={`fixed min-w-[332px] p-4 xs:w-fit flex flex-col gap-2 right-1/2 translate-x-1/2 md:right-0 md:-translate-x-0 text-text transition-all !duration-500 z-50 ${isTouchDevice ? containerSwipeStyles : '-bottom-[4.5rem] group hover:bottom-0 group'}`}>
         <span ref={audioLabel} className={`w-full text-[.85rem] xs:text-sm md:text-base text-center absolute left-1/2 -translate-x-1/2 ${isTouchDevice ? labelSwipeStyles : 'top-4 opacity-0 group-hover:-top-4 group-hover:opacity-100' } transition-all !duration-500 select-none cursor-default pointer-events-none`}>
           Click play for an amazing experience

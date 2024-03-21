91a040b163db2d7abd5d2e05380f6add0e858a4c
dpaulos6 <itzframepvp@outlook.com>
2024-03-21 11:13:25 +0000

    Improvements

--- src/components/BackgroundAudio.tsx
+++ src/components/BackgroundAudio.tsx
@@ -46,10 +46,10 @@ const BackgroundAudio: React.FC = () => {
   const handlers = useSwipeable({
     onSwipedUp: () => {
       if (audioContainer.current) {
-        setContainerSwipeStyles('bottom-0')
+        setContainerSwipeStyles('bottom-0 pt-0')
       }
       if (audioLabel.current) {
-        setLabelSwipeStyles('-top-4 opacity-100')
+        setLabelSwipeStyles('-top-8 opacity-100')
       }
     },
     onSwipedDown: () => {
@@ -67,12 +67,8 @@ const BackgroundAudio: React.FC = () => {
     <>
       <div className='flex md:hidden w-full h-1/3 absolute bottom-0 z-50' {...handlers}></div>
       <div ref={audioContainer} className={`fixed min-w-[332px] p-4 xs:w-fit flex flex-col gap-2 right-1/2 translate-x-1/2 md:right-0 md:-translate-x-0 text-text transition-all !duration-500 z-50 ${isTouchDevice ? containerSwipeStyles : '-bottom-[4.5rem] group hover:bottom-0 group'}`}>
-        <span ref={audioLabel} className={`w-full text-[.85rem] xs:text-sm md:text-base text-center absolute left-1/2 -translate-x-1/2 ${isTouchDevice ? labelSwipeStyles : 'top-4 opacity-0 group-hover:-top-4 group-hover:opacity-100' } transition-all !duration-500 select-none cursor-default`}>
-          Click{' '}
-          <button className="font-bold text-primary" onClick={playMusic}>
-            play
-          </button>{' '}
-          for an amazing experience
+        <span ref={audioLabel} className={`w-full text-[.85rem] xs:text-sm md:text-base text-center absolute left-1/2 -translate-x-1/2 ${isTouchDevice ? labelSwipeStyles : 'top-4 opacity-0 group-hover:-top-4 group-hover:opacity-100' } transition-all !duration-500 select-none cursor-default pointer-events-none`}>
+          Click play for an amazing experience
         </span>
         <AudioPlayer src="bgmusic.mp3" />
       </div>

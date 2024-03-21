8fba8d6b4e7f1b91e1c97834508ca6eb82a65fc4
dpaulos6 <itzframepvp@outlook.com>
2024-03-21 11:03:59 +0000

    Fix and Improvement on Swipe Gesture

--- src/components/BackgroundAudio.tsx
+++ src/components/BackgroundAudio.tsx
@@ -7,8 +7,8 @@ import config from 'next/config'
 const BackgroundAudio: React.FC = () => {
   const [isTouchDevice, setIsTouchDevice] = useState(false)
   const [windowWidth, setWindowWidth] = useState<number | undefined>(undefined)
-  const [containerSwipeStyles, setContainerSwipeStyles] = useState('')
-  const [labelSwipeStyles, setLabelSwipeStyles] = useState('')
+  const [containerSwipeStyles, setContainerSwipeStyles] = useState('-bottom-[4.5rem] group pointer-events-none')
+  const [labelSwipeStyles, setLabelSwipeStyles] = useState('top-4 opacity-0')
   const audioContainer = React.useRef<HTMLDivElement>(null)
   const audioLabel = React.useRef<HTMLDivElement>(null)
 
@@ -54,10 +54,10 @@ const BackgroundAudio: React.FC = () => {
     },
     onSwipedDown: () => {
       if (audioContainer.current) {
-        setContainerSwipeStyles('')
+        setContainerSwipeStyles('-bottom-[4.5rem] group pointer-events-none')
       }
       if (audioLabel.current) {
-        setLabelSwipeStyles('')
+        setLabelSwipeStyles('top-4 opacity-0')
       }
     },
     ...config,
@@ -66,8 +66,8 @@ const BackgroundAudio: React.FC = () => {
   return (
     <>
       <div className='flex md:hidden w-full h-1/3 absolute bottom-0 z-50' {...handlers}></div>
-      <div ref={audioContainer} className={`fixed min-w-[332px] p-4 xs:w-fit flex flex-col gap-2 right-1/2 translate-x-1/2 ${windowWidth && windowWidth <= 768 ? '-bottom-[4.5rem] group' : ''} md:right-0 md:-translate-x-0 text-text transition-all !duration-500 z-50 ${isTouchDevice ? containerSwipeStyles : '-bottom-[4.5rem] group hover:bottom-0 group'}`}>
-        <span ref={audioLabel} className={`w-full text-[.85rem] xs:text-sm md:text-base text-center absolute left-1/2 -translate-x-1/2 ${windowWidth && windowWidth <= 768 ? 'top-4 opacity-0' : ''} ${isTouchDevice ? labelSwipeStyles : 'top-4 opacity-0 group-hover:-top-4 group-hover:opacity-100' } transition-all !duration-500 select-none cursor-default`}>
+      <div ref={audioContainer} className={`fixed min-w-[332px] p-4 xs:w-fit flex flex-col gap-2 right-1/2 translate-x-1/2 md:right-0 md:-translate-x-0 text-text transition-all !duration-500 z-50 ${isTouchDevice ? containerSwipeStyles : '-bottom-[4.5rem] group hover:bottom-0 group'}`}>
+        <span ref={audioLabel} className={`w-full text-[.85rem] xs:text-sm md:text-base text-center absolute left-1/2 -translate-x-1/2 ${isTouchDevice ? labelSwipeStyles : 'top-4 opacity-0 group-hover:-top-4 group-hover:opacity-100' } transition-all !duration-500 select-none cursor-default`}>
           Click{' '}
           <button className="font-bold text-primary" onClick={playMusic}>
             play

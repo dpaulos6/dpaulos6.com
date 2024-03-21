812113b2f15c3e45a6e4e2055e537713492a3d24
dpaulos6 <itzframepvp@outlook.com>
2024-03-21 10:43:13 +0000

    Swipe Gestures on smaller Screens

--- src/components/BackgroundAudio.tsx
+++ src/components/BackgroundAudio.tsx
@@ -6,6 +6,9 @@ import config from 'next/config'
 
 const BackgroundAudio: React.FC = () => {
   const [isTouchDevice, setIsTouchDevice] = useState(false)
+  const [windowWidth, setWindowWidth] = useState<number | undefined>(undefined)
+  const [containerSwipeStyles, setContainerSwipeStyles] = useState('')
+  const [labelSwipeStyles, setLabelSwipeStyles] = useState('')
   const audioContainer = React.useRef<HTMLDivElement>(null)
   const audioLabel = React.useRef<HTMLDivElement>(null)
 
@@ -15,12 +18,21 @@ const BackgroundAudio: React.FC = () => {
       audio.volume = 0.1
     }
 
-    const onTouchStart = () => {
-      setIsTouchDevice(true)
+    // Function to handle window resize and update states accordingly
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth)
+      setIsTouchDevice(window.innerWidth <= 768)
     }
-    window.addEventListener('touchstart', onTouchStart)
+
+    // Add event listener for window resize
+    window.addEventListener('resize', handleResize)
+
+    // Call handleResize once when the component mounts to set initial values
+    handleResize()
+
+    // Cleanup function to remove event listener when the component unmounts
     return () => {
-      window.removeEventListener('touchstart', onTouchStart)
+      window.removeEventListener('resize', handleResize)
     }
   }, [])
 
@@ -34,20 +46,18 @@ const BackgroundAudio: React.FC = () => {
   const handlers = useSwipeable({
     onSwipedUp: () => {
       if (audioContainer.current) {
-        audioContainer.current.style.bottom = '0'
+        setContainerSwipeStyles('bottom-0')
       }
       if (audioLabel.current) {
-        audioLabel.current.style.top = '-1rem'
-        audioLabel.current.style.opacity = '100'
+        setLabelSwipeStyles('-top-4 opacity-100')
       }
     },
     onSwipedDown: () => {
       if (audioContainer.current) {
-        audioContainer.current.style.bottom = '-4.5rem'
+        setContainerSwipeStyles('')
       }
       if (audioLabel.current) {
-        audioLabel.current.style.top = '1rem'
-        audioLabel.current.style.opacity = '0'
+        setLabelSwipeStyles('')
       }
     },
     ...config,
@@ -56,8 +66,8 @@ const BackgroundAudio: React.FC = () => {
   return (
     <>
       <div className='flex md:hidden w-full h-1/3 absolute bottom-0 z-50' {...handlers}></div>
-      <div ref={audioContainer} className={`fixed min-w-[332px] p-4 xs:w-fit flex flex-col gap-2 right-1/2 translate-x-1/2 -bottom-[4.5rem] md:right-0 md:-translate-x-0 text-text ${!isTouchDevice ? 'hover:bottom-0 group' : '' } transition-all !duration-500 z-50`}>
-        <span ref={audioLabel} className={`w-full text-[.85rem] xs:text-sm md:text-base text-center absolute left-1/2 -translate-x-1/2 top-4 opacity-0 ${!isTouchDevice ? 'group-hover:-top-4 group-hover:opacity-100' : '' } transition-all !duration-500 select-none cursor-default`}>
+      <div ref={audioContainer} className={`fixed min-w-[332px] p-4 xs:w-fit flex flex-col gap-2 right-1/2 translate-x-1/2 -bottom-[4.5rem] group md:right-0 md:-translate-x-0 text-text transition-all !duration-500 z-50 ${isTouchDevice ? containerSwipeStyles : 'hover:bottom-0 group'}`}>
+        <span ref={audioLabel} className={`w-full text-[.85rem] xs:text-sm md:text-base text-center absolute left-1/2 -translate-x-1/2 top-4 opacity-0 ${isTouchDevice ? labelSwipeStyles : 'group-hover:-top-4 group-hover:opacity-100' } transition-all !duration-500 select-none cursor-default`}>
           Click{' '}
           <button className="font-bold text-primary" onClick={playMusic}>
             play

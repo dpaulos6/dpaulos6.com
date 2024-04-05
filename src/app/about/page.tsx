04fdf65b93ffe42df1c818c18be599c4117e3797
dpaulos6 <itzframepvp@outlook.com>
2024-04-05 23:48:36 +0100

    about init

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,18 +1,30 @@
 import '@/app/globals.css'
 import Link from 'next/link'
+import Image from 'next/image'
 
 export default function Page() {
   return (
     <>
       <div className="w-screen h-screen flex flex-col gap-2 items-center justify-center text-text">
-        <span className="text-5xl">This is still being built</span>
-        <span className="text-lg">
-          You can join my{' '}
-          <Link href="https://discord.gg/Y7ujpKmmma" className="text-primary">
-            discord
-          </Link>{' '}
-          if you wanna see its progress
-        </span>
+        <div className="flex gap-16 justify-between items-center w-full max-w-7xl">
+          <div className="flex flex-col gap-6">
+            <Image
+              src="/dpaulos6_nobg.png"
+              alt="A photo of me."
+              width={9999}
+              height={9999}
+              className="w-96 h-auto rounded-b-full"
+            />
+          </div>
+          <div className="flex flex-col items-center">
+            <span className="text-2xl">
+              My name is{' '}
+              <b className="font-normal text-primary">Diogo Paulos</b>, I&apos;m
+              20 years old and I&apos;m a Portuguese{' '}
+              <b className="font-normal text-primary">fullstack developer</b>.
+            </span>
+          </div>
+        </div>
       </div>
     </>
   )

b4efde216f6169c2fb79d0d188a8a6490d248528
dpaulos6 <itzframepvp@outlook.com>
2024-04-08 15:40:10 +0100

    Update gradient-background.tsx

--- src/components/gradient-background.tsx
+++ src/components/gradient-background.tsx
@@ -1,38 +1,4 @@
-'use client'
-import { useRef, useState } from 'react'
-
 const GradientBackground = () => {
-  const divRef = useRef<HTMLDivElement>(null)
-  const [isFocused, setIsFocused] = useState(false)
-  const [position, setPosition] = useState({ x: 0, y: 0 })
-  const [opacity, setOpacity] = useState(0)
-
-  const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
-    if (!divRef.current || isFocused) return
-
-    const div = divRef.current
-    const rect = div.getBoundingClientRect()
-
-    setPosition({ x: e.clientX - rect.left, y: e.clientY - rect.top })
-  }
-
-  const handleFocus = () => {
-    setIsFocused(true)
-    setOpacity(1)
-  }
-
-  const handleBlur = () => {
-    setIsFocused(false)
-    setOpacity(0)
-  }
-
-  const handleMouseEnter = () => {
-    setOpacity(1)
-  }
-
-  const handleMouseLeave = () => {
-    setOpacity(0)
-  }
   return (
     <>
       <div className="absolute inset-0 h-screen w-screen bg-background bg-[linear-gradient(to_right,rgb(var(--background-hover))_1px,transparent_1px),linear-gradient(to_bottom,rgb(var(--background-hover))_1px,transparent_1px)] bg-[size:2.5rem_2.5rem] opacity-40 pointer-events-none">

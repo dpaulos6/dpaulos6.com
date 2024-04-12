b6fbe99c6655aed85973aaa0990efe5a88610767
dpaulos6 <itzframepvp@outlook.com>
2024-04-12 14:21:53 +0100

    formatting and fixes

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -44,7 +44,9 @@ export default function Page() {
         <div className="w-full h-16 pointer-events-none" />
         <div className="w-full max-w-7xl flex flex-col items-center mb-40">
           <div className="w-fit relative mb-32 group">
-            <span className="text-5xl md:text-6xl px-24 pt-8">Who am I?</span>
+            <span className="text-5xl md:text-6xl px-24 pt-8 whitespace-nowrap">
+              Who am I?
+            </span>
             <span className="absolute top-0 -translate-y-1/2 left-1/2 -translate-x-1/2 text-7xl md:text-8xl whitespace-nowrap text-neutral-200/50 -z-10 transition-all md:group-hover:opacity-0">
               Who am I?
             </span>
@@ -117,7 +119,9 @@ export default function Page() {
             </span>
           </div>
           <div className="flex md:hidden w-fit relative mb-32 text-center">
-            <span className="text-5xl md:text-6xl">What stack?</span>
+            <span className="text-5xl md:text-6xl whitespace-nowrap">
+              What stack?
+            </span>
             <span className="absolute top-0 -translate-y-1/2 left-1/2 -translate-x-1/2 text-7xl md:text-8xl whitespace-nowrap text-neutral-200/50 -z-10">
               What stack?
             </span>

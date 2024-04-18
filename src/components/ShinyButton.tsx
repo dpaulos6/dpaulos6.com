5132e7cb04304ba5c2870422a19a0e7245720b5f
Diogo Paulos <51181228+dpaulos6@users.noreply.github.com>
2024-04-18 22:12:18 +0100

    Updating ShinyButton

--- src/components/ShinyButton.tsx
+++ src/components/ShinyButton.tsx
@@ -1,24 +1,29 @@
-import { motion } from 'framer-motion'
+import { motion, Variants } from 'framer-motion'
 
-interface props {
+interface Props {
   children: React.ReactNode
   className?: string
 }
 
-const ShinyButton = ({ children, className }: props) => {
+const ShinyButton = ({ children, className }: Props) => {
+  const buttonVariants: Variants = {
+    initial: { '--x': '100%' },
+    animate: { '--x': '-100%' }
+  }
+
   return (
     <motion.button
-      initial={{ '--x': '100%' }}
-      animate={{ '--x': '-100%' }}
+      initial="initial"
+      animate="animate"
       transition={{
         repeat: Infinity,
         repeatType: 'loop',
-        // repeatDelay: 1,
         type: 'spring',
         stiffness: 20,
-        damping: 15,
+        damping: 20,
         mass: 2
       }}
+      variants={buttonVariants}
       className={`px-10 py-2 rounded-md relative bg-background-menu ${className}`}
     >
       {children}

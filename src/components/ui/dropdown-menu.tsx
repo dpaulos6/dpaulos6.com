4b70e493a90ee3a5630612907ad4c02ad3feecd7
dpaulos6 <itzframepvp@outlook.com>
2024-06-24 17:23:41 +0100

    fixes

--- src/components/ui/dropdown-menu.tsx
+++ src/components/ui/dropdown-menu.tsx
@@ -160,7 +160,7 @@ const DropdownMenuSeparator = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <DropdownMenuPrimitive.Separator
     ref={ref}
-    className={cn('-mx-1 my-1 h-px bg-stone-100', className)}
+    className={cn('-mx-1 my-1 h-px bg-background-border', className)}
     {...props}
   />
 ))

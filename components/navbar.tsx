51c43f5a1169a4f14c870dac96fc3250a5a0d754
dpaulos6 <itzframepvp@outlook.com>
2024-09-20 15:41:56 +0100

    feat: update logo

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,32 +2,28 @@
 
 import { Moon, Sun } from 'lucide-react'
 import { useTheme } from 'next-themes'
+import { DPLogo } from '@/icons/dp'
 import Link from 'next/link'
 
 export function Navbar() {
   const { theme, setTheme } = useTheme()
 
   return (
-    <nav className="w-full h-fit flex justify-center px-8 py-4 z-50 select-none">
-      <div className="flex w-full max-w-7xl justify-between">
+    <nav className="w-full h-fit flex justify-center p-8 z-50 select-none">
+      <div className="flex w-full max-w-7xl justify-between items-center">
         <Link
           href="/"
           className="text-2xl group rounded-xl"
         >
-          <span className="text-text group-hover:text-primary transition duration-300">
-            dpaulos
-          </span>
-          <span className="text-primary group-hover:text-text transition duration-300">
-            6
-          </span>
+          <DPLogo className="logo-hover w-12 h-auto" />
         </Link>
         <button
-          className="text-text p-2 rounded-xl hover:bg-text/5 transition"
+          className="w-12 h-12 flex items-center justify-center text-text p-2 rounded-xl hover:bg-text/5 hover:text-primary transition"
           onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
         >
           {theme === 'light' ?
-            <Moon />
-          : <Sun />}
+            <Moon className="w-7 h-7" />
+          : <Sun className="w-7 h-7" />}
         </button>
       </div>
     </nav>
---
+++ icons/dp.tsx
@@ -0,0 +1,49 @@
+import { SVGProps } from 'react'
+import React from 'react'
+
+export function DPLogo(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 206.08 178.21"
+      width="1em"
+      height="1em"
+      {...props}
+    >
+      <defs></defs>
+      <g
+        id="Layer_2"
+        data-name="Layer 2"
+      >
+        <g id="svg495991">
+          <g id="logo-group">
+            <g id="logo-center">
+              <g id="title">
+                <g id="path496000">
+                  <path
+                    fill="hsl(var(--dp-primary))"
+                    d="M0,114.85H40.59c24,0,32.68-8.71,32.68-32.67V0H97V82.18c0,35.44-21,56.43-56.44,56.43H0Z"
+                  />
+                  <path
+                    fill="hsl(var(--dp-accent))"
+                    d="M0,96C0,60.59,21,39.6,56.43,39.6h5V63.37h-5c-24,0-32.67,8.71-32.67,32.67V103H0Z"
+                  />
+                </g>
+                <g id="path496002">
+                  <path
+                    fill="hsl(var(--dp-primary))"
+                    d="M206.08,63.36H165.49c-24,0-32.67,8.71-32.67,32.67v82.18H109.05V96c0-35.44,21-56.43,56.44-56.43h40.59V63.36Z"
+                  />
+                  <path
+                    fill="hsl(var(--dp-accent))"
+                    d="M144.7,114.85h5c24,0,32.67-8.72,32.67-32.68V75.24h23.76v6.93c0,35.45-21,56.44-56.43,56.44h-5V114.85Z"
+                  />
+                </g>
+              </g>
+            </g>
+          </g>
+        </g>
+      </g>
+    </svg>
+  )
+}

82a519a34f651aaf72b5ca0da80140c5f696e34b
Diogo Paulos <itzframepvp@outlook.com>
2024-09-23 12:53:20 +0100

    feat: cv

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Moon, Sun } from 'lucide-react'
+import { Download, Moon, Sun } from 'lucide-react'
 import { useTheme } from 'next-themes'
 import { DPLogo } from '@/icons/dp'
 import Link from 'next/link'
@@ -9,22 +9,33 @@ export function Navbar() {
   const { theme, setTheme } = useTheme()
 
   return (
-    <nav className="w-full h-fit flex justify-center p-8 z-50 select-none">
+    <nav className="w-full h-fit flex justify-center p-8 z-50 select-none ">
       <div className="flex w-full max-w-7xl justify-between items-center">
         <Link
           href="/"
           className="text-2xl group rounded-xl"
         >
-          <DPLogo className="logo-hover w-12 h-auto" />
+          <DPLogo className="logo-hover w-10 md:w-12 h-auto" />
         </Link>
-        <button
-          className="w-12 h-12 flex items-center justify-center text-text p-2 rounded-xl hover:bg-text/5 hover:text-primary transition"
-          onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
-        >
-          {theme === 'light' ?
-            <Moon className="w-7 h-7" />
-          : <Sun className="w-7 h-7" />}
-        </button>
+        <div className="flex gap-2">
+          <a
+            href="/downloads/dpauloscv.pdf"
+            className="h-10 md:h-12 flex gap-2 items-center justify-center text-text p-2 rounded-xl hover:bg-text/5 hover:text-primary transition whitespace-nowrap"
+            download
+          >
+            <Download className="w-7 h-7" />
+            <span className="text-lg md:block hidden">Download CV</span>
+            <span className="text-lg md:hidden">CV</span>
+          </a>
+          <button
+            className="w-10 h-10 md:w-12 md:h-12 flex items-center justify-center text-text p-2 rounded-xl hover:bg-text/5 hover:text-primary transition"
+            onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+          >
+            {theme === 'light' ?
+              <Moon className="w-7 h-7" />
+            : <Sun className="w-7 h-7" />}
+          </button>
+        </div>
       </div>
     </nav>
   )

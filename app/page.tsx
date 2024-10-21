e003f677ac364f720fb973204cc23a171e2e63de
Diogo Paulos <itzframepvp@outlook.com>
2024-10-22 00:04:08 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import './globals.css'
 
 export default function Home() {
   return (
-    <main className="flex-1 flex flex-col gap-20 xl:gap-40 items-center text-text overflow-x-hidden pt-32 text-green-300 dark:text-green-500">
+    <main className="flex-1 flex flex-col gap-20 xl:gap-40 items-center text-text overflow-x-hidden pt-32 bg-green-300 dark:bg-green-500">
       <AboutSection />
       <KnowledgeSection />
       <ProjectsSection />

2ed5095913c5636843b5d8e2b903da7062ea469b
ForGetFulSkyBro <47897305+forgetfulskybro@users.noreply.github.com>
2025-01-02 13:45:29 -0500

    Consistent projects & contributions

--- components/projects-section.tsx
+++ components/projects-section.tsx
@@ -37,7 +37,7 @@ export function ProjectsSection() {
     <div className="w-screen h-auto flex flex-col gap-20 items-center text-text py-12">
       <div className="flex flex-col items-center gap-12 pt-12 w-full">
         <Title title="Projects" />
-        <div className="flex flex-col items-center gap-6 w-full max-w-5xl p-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6 w-full max-w-5xl p-8">
           <TooltipProvider delayDuration={0}>
             {projects
               .slice()
@@ -45,21 +45,19 @@ export function ProjectsSection() {
               .map((project, i) => (
                 <div
                   key={i}
-                  className={`flex justify-center items-center w-fit lg:w-full p-[2px] rounded-[0.9rem] overflow-hidden transition-all group relative`}
+                  className={`flex justify-center items-center w-full  p-[2px] rounded-[0.9rem] overflow-hidden transition-all group relative`}
                 >
                   <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-br from-primary to-accent opacity-0 group-hover:opacity-50 transition pointer-events-none "></div>
-                  <div className="flex flex-col max-w-96 md:max-w-xl lg:max-w-none lg:flex-row items-center w-full h-full gap-4 md:gap-6 lg:gap-8 p-6 rounded-xl bg-background-menu z-10">
-                    <div className="flex basis-3/5 w-full h-fit aspect-[5/3] relative">
+                  <div className="flex flex-col w-full h-full gap-4 p-6 rounded-xl bg-background-menu z-10">
+                    <div className="relative w-full h-32 md:h-40 lg:h-48">
                       <Image
                         src={
-                          project.thumbnail ?
-                            project.thumbnail
-                          : 'https://www.sbrv.org/wp-content/uploads/2019/11/preview.png'
+                          project.thumbnail ||
+                          'https://www.sbrv.org/wp-content/uploads/2019/11/preview.png'
                         }
                         alt={project.name + ' Thumbnail'}
-                        width={9999}
-                        height={9999}
-                        className="flex object-cover object-center select-none rounded-xl border border-text/10"
+                        fill
+                        className="object-cover rounded-xl border border-text/10"
                       />
                       <Badge
                         className={cn(
@@ -70,17 +68,16 @@ export function ProjectsSection() {
                         <span>{project.tag.label}</span>
                       </Badge>
                     </div>
-                    <div className="w-full col-span-2 flex flex-col gap-2 py-2 pr-2 h-full">
+                    <div className="flex flex-col gap-2 py-2 pr-2">
                       <div className="flex items-center">
                         <span className="text-xl md:text-2xl lg:text-3xl mr-2">
                           {project.name}
                         </span>
-                        {project.url && !project.currentWebsite ?
+                        {project.url && !project.currentWebsite && (
                           <Tooltip>
                             <TooltipTrigger tabIndex={-1}>
                               <Link
                                 href={project.url}
-                                className=""
                                 target="_blank"
                               >
                                 <LinkIcon className="p-2 w-10 rounded-md h-full aspect-square lg:opacity-0 lg:group-hover:opacity-100 hover:bg-primary/15 hover:text-primary transition" />
@@ -90,13 +87,12 @@ export function ProjectsSection() {
                               <p>Visit URL</p>
                             </TooltipContent>
                           </Tooltip>
-                        : null}
-                        {project.github ?
+                        )}
+                        {project.github && (
                           <Tooltip>
                             <TooltipTrigger tabIndex={-1}>
                               <Link
                                 href={project.github}
-                                className=""
                                 target="_blank"
                               >
                                 <GithubIcon className="p-2 w-10 rounded-md h-full aspect-square lg:opacity-0 lg:group-hover:opacity-100 hover:bg-primary/15 hover:text-primary transition" />
@@ -106,17 +102,17 @@ export function ProjectsSection() {
                               <p>View on GitHub</p>
                             </TooltipContent>
                           </Tooltip>
-                        : null}
+                        )}
                       </div>
                       <span
                         className={`${isExpanded[i] ? 'h-auto line-clamp-none overflow-auto' : 'lg:overflow-ellipsis lg:line-clamp-3'} text-xs md:text-sm lg:text-base leading-[1.75] mb-2`}
                       >
                         {project.description}
                       </span>
-                      <div className="flex flex-wrap w-full gap-4 mt-auto select-none ">
-                        {project.technologies.map((tech, i) => (
-                          <Tooltip key={i}>
-                            <TooltipTrigger className="h-fit rounded-md ">
+                      <div className="flex flex-wrap gap-4 mt-auto select-none">
+                        {project.technologies.map((tech, j) => (
+                          <Tooltip key={j}>
+                            <TooltipTrigger className="h-fit rounded-md flex items-center justify-center p-1 rounded-md transition hover:bg-primary/15 hover:text-primary">
                               <tech.icon className="w-6 md:w-8 h-full aspect-square" />
                             </TooltipTrigger>
                             <TooltipContent>
@@ -124,7 +120,7 @@ export function ProjectsSection() {
                             </TooltipContent>
                           </Tooltip>
                         ))}
-                        {project.description.length > 200 && (
+                        {project.description.length > 160 && (
                           <button
                             onClick={() => toggleProjectExpanded(i)}
                             className="hidden lg:flex ml-auto mr-4 px-4 py-2 rounded-lg hover:bg-primary/15 opacity-0 pointer-events-none group-hover:opacity-100 group-hover:pointer-events-auto hover:text-primary transition"
@@ -154,9 +150,8 @@ export function ProjectsSection() {
                   <div className="flex basis-3/5 w-full h-fit aspect-[5/3] relative">
                     <Image
                       src={
-                        contribution.thumbnail ?
-                          contribution.thumbnail
-                        : 'https://www.sbrv.org/wp-content/uploads/2019/11/preview.png'
+                        contribution.thumbnail ||
+                        'https://www.sbrv.org/wp-content/uploads/2019/11/preview.png'
                       }
                       alt={contribution.name + ' Thumbnail'}
                       width={9999}
@@ -174,7 +169,7 @@ export function ProjectsSection() {
                       <span className="text-xl md:text-2xl lg:text-3xl mr-2">
                         {contribution.name}
                       </span>
-                      {contribution.url ?
+                      {contribution.url && (
                         <Tooltip>
                           <TooltipTrigger tabIndex={-1}>
                             <Link
@@ -188,8 +183,8 @@ export function ProjectsSection() {
                             <p>Visit URL</p>
                           </TooltipContent>
                         </Tooltip>
-                      : null}
-                      {contribution.github ?
+                      )}
+                      {contribution.github && (
                         <Tooltip>
                           <TooltipTrigger tabIndex={-1}>
                             <Link
@@ -203,7 +198,7 @@ export function ProjectsSection() {
                             <p>View on GitHub</p>
                           </TooltipContent>
                         </Tooltip>
-                      : null}
+                      )}
                     </div>
                   </div>
                 </div>
